9f84fdd7dd4a56e59a89a6aa46d175a7ca03ab16
Chris Diebold <cdiebold2012@my.fit.edu>
2015-05-04 19:39:59 -0400

    added some buttons

--- GeoQuiz/app/src/main/java/android/learning/diebold/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/android/learning/diebold/geoquiz/QuizActivity.java
@@ -1,17 +1,40 @@
 package android.learning.diebold.geoquiz;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class QuizActivity extends ActionBarActivity {
+public class QuizActivity extends Activity {
+
+	private Button mTrueButton;
+	private Button mFalseButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
+
+		mTrueButton = (Button)findViewById(R.id.true_button);
+		mFalseButton = (Button)findViewById(R.id.false_button);
+
+		mTrueButton.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
 	}
 
 	@Override
